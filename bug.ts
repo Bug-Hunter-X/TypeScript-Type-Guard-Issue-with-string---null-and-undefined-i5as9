85e5dc4f22e6a7b91b1c24a1d2682c783b59a646
BugHunter-X <askzenai@gmail.com>
2024-12-22 23:54:05 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,12 @@
+function printName(name: string | null): void {
+  if (name) {
+    console.log(name.toUpperCase());
+  } else {
+    console.log('No name provided');
+  }
+}
+
+printName(null); // Works fine
+printName(undefined); // Error: Object is possibly 'undefined'
+
+// this is the bug because undefined is also a valid type in the function argument signature
